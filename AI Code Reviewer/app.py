03d3b18567d34151e8d86d35441453a1dcd736c7
Naga Anweesh Reddy Janga <114130799+Arjun1443@users.noreply.github.com>
2025-02-12 16:00:03 +0530

    Add files via upload

---
+++ AI Code Reviewer/app.py
@@ -0,0 +1,28 @@
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyB3mOyPFo7R782S_ZwQgX5Oks4ES8V3UdE")
+# initializing the  model
+model = genai.GenerativeModel(model_name = "models/gemini-2.0-flash-exp")
+
+from IPython.display import Markdown 
+import streamlit as st
+
+st.title("Code Reviewer AI")
+st.write("Ask your coding-related queries!")
+
+
+
+sys_prompt = """You are a helpful codereviwerer. You can resolve coding related queries.
+In case if someone ask queries whicha re not relevance to data science, politely tell
+them to ask queries only"""
+model = genai.GenerativeModel(model_name = "models/gemini-2.0-flash-exp", system_instruction = sys_prompt)
+
+
+user_prompt = st.text_area("Enter your python code")
+
+if st.button("Generate") == True:
+        st.ballons()
+
+
+
+response = model.generate_content(user_prompt)
+st.markdown(response.text)
